8124820346d6d755ae9ee42d7e3beb59f51185fd
AndreyRogalev <rogalevandrey@gmail.com>
2025-05-01 08:08:43 +0000

    new full project

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -30,6 +30,7 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup; // Добавлен импорт
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
